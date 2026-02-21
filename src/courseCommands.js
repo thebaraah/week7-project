5c6dacfd7a141f9284c650ce213a16326529bd21
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-21 10:41:18 +0100

    Added the addCourse function

--- src/courseCommands.js
+++ src/courseCommands.js
@@ -1,7 +1,60 @@
 import { saveCourseData, loadCourseData } from './storage.js';
 
-function addCourse() {
+function addCourse(args) {
   // TODO: Implement logic
+  if (args.length !== 2) {
+    console.log('ERROR: Must provide course name and start date');
+    return;
+  }
+  //check format
+  const name = args[0];
+  const startDate = args[1];
+
+  if (!isGoodDate) {
+    console.log('ERROR: Invalid start date. Must be in the yyyy-mm-dd format');
+    return;
+  }
+
+  const courses = loadCourseData();
+  //create unique id and try 20 times
+  let id;
+  let isUnique = false;
+
+  for (let tryCount = 0; tryCount < 20; tryCount++) {
+    id = Math.floor(Math.random() * 1000);
+    //look if id is used
+    isUnique = true;
+    for (let i = 0; i < courses.length; i++) {
+      if (courses[i].id === id) {
+        isUnique = false;
+        break;
+      }
+    }
+    //stop looking if id is unique
+    if (isUnique) {
+      break;
+    }
+  }
+  if (!isUnique) {
+    id = 0;
+    for (let i = 0; i < courses.length; i++) {
+      if (courses[i].id > id) {
+        id = courses[i].id;
+      }
+    }
+    id = id + 1;
+    if (id > 9999) id = 9999;
+  }
+  //adding a new course
+  const newCourse = {
+    id: id,
+    name: name,
+    startDate: startDate,
+  };
+  courses.push(newCourse);
+  saveCourseData(courses);
+
+  console.log('CREATED: ${id} ${name} ${startDate}');
 }
 
 function updateCourse() {
